0131bbd88515409fc729743cd0f85a19f3226bbb
Madi Rysbekov <hooizit@gmail.com>
2018-01-08 02:35:01 +0600

    js added

---
+++ js/fullName/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("First name:");
+var lastName = prompt("Last name:");
+var userAge = prompt("Age:");
+
+var fullName = firstName + " " + lastName;
+console.log("Your full name is " + fullName);
+console.log("You are " + userAge + " years old");
