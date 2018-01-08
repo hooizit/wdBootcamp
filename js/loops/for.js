df8d4a9dd85c8219e244acd3f0f2859e1ddbb881
Madi Rysbekov <hooizit@gmail.com>
2018-01-08 17:36:16 +0600

    loops

---
+++ js/loops/for.js
@@ -0,0 +1,35 @@
+for (var var i = 0; i < 16; i+=8) {
+  console.log(i);
+}
+
+var str = "ahselljlgo";
+
+for(var i = 1; i < str.length; i+=2) {
+  console.log(str[i]);
+}
+
+// Print all numbers between -10 and 19
+for(var i = -10; i < 20; i++) {
+  console.log(i);
+}
+// Print all even numbers between 10 and 40
+for(var i = 10; i < 41; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// Print all odd numbers between 300 and 333
+for(i = 300; i < 334; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+// Print all numbers divisible by 5 and 3 between 5 and 50
+var num = 5;
+for(i = 5; i < 51; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    console.log(i);
+  }
+}
--- js/loops/while.js
+++ js/loops/while.js
@@ -5,13 +5,13 @@ while(num <= 20) {
   }
   num++;
 }
-
+// Print all numbers between -10 and 19
 var num = 19;
 while(num >= -10) {
   console.log(num);
   num--;
 }
-
+// Print all even numbers between 10 and 40
 var num = 10;
 while(num <= 40) {
   if (num % 2 === 0) {
@@ -19,7 +19,7 @@ while(num <= 40) {
   }
   num++;
 }
-
+// Print all odd numbers between 300 and 333
 var num = 300;
 while(num <= 333) {
   if (num % 2 != 0) {
@@ -27,7 +27,7 @@ while(num <= 333) {
   }
   num++;
 }
-
+// Print all numbers divisible by 5 and 3 between 5 and 50
 var num = 5;
 while(num <= 50) {
   if (num % 3 === 0 || num % 5 === 0) {
