66b261c3445b0163c6c7242bcdc31a99f8b0fff9
Madi Rysbekov <hooizit@gmail.com>
2018-01-08 16:42:54 +0600

    while

---
+++ js/loops/while.js
@@ -0,0 +1,37 @@
+var num = 1;
+while(num <= 20) {
+  if (num % 4 ===0) {
+    console.log(num);
+  }
+  num++;
+}
+
+var num = 19;
+while(num >= -10) {
+  console.log(num);
+  num--;
+}
+
+var num = 10;
+while(num <= 40) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+  num++;
+}
+
+var num = 300;
+while(num <= 333) {
+  if (num % 2 != 0) {
+    console.log(num);
+  }
+  num++;
+}
+
+var num = 5;
+while(num <= 50) {
+  if (num % 3 === 0 || num % 5 === 0) {
+    console.log(num);
+  }
+  num++;
+}
