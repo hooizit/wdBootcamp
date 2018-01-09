6215e8d17d345cb5a535cc53f4715d29eeb75c0f
Madi Rysbekov <hooizit@gmail.com>
2018-01-09 21:29:57 +0600

    0.1: Refactored

--- js/todoApp/list.js
+++ js/todoApp/list.js
@@ -1,29 +1,45 @@
 var todos = ["todo 1", "todo 2"];
 
-var input = prompt("What you like to do? NOTE: l - list todo's; n - new todo; q - quit");
+var input = prompt(
+  "What you like to do? NOTE: l - list todo's; n - new todo; q - quit"
+);
 
 while (input !== "q") {
   //handle input
   if (input === "l") {
-    console.log("-----------------");
-    todos.forEach(function(todo, i){
-      console.log(i + ": " + todo);
-    });
-    console.log("-----------------");
+    listTodos();
   } else if (input === "n") {
-    //ask new todo
-    var newTodo = prompt("Enter new todo");
-    //add to todos array
-    todos.push(newTodo);
-    console.log("Added Todo")
+    addTodo();
   } else if (input === "r") {
-    //ask for index of todo to be removed
-    var index = prompt("Enter index of todo to remove");
-    //delet that todo
-    todos.splice(index,1);
-    console.log("Removed Todo")
+    deleteTodo();
   }
   //ask again for new input
-  input = prompt("What you like to do? NOTE: l - list todo's; n - new todo; q - quit");
+  input = prompt(
+    "What you like to do? NOTE: l - list todo's; n - new todo; q - quit"
+  );
 }
 console.log("quit");
+
+function listTodos() {
+  console.log("-----------------");
+  todos.forEach(function(todo, i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("-----------------");
+}
+
+function addTodo() {
+  //ask new todo
+  var newTodo = prompt("Enter new todo");
+  //add to todos array
+  todos.push(newTodo);
+  console.log("Added Todo");
+}
+
+function deleteTodo() {
+  //ask for index of todo to be removed
+  var index = prompt("Enter index of todo to remove");
+  //delet that todo
+  todos.splice(index, 1);
+  console.log("Removed Todo");
+}
